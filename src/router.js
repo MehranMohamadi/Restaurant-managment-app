f566bb58fbbd9c51e34d5f5245c76dc754f86fd3
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-09-11 21:25:01 +0430

    add router

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import {createRouter, createWebHistory} from 'vue-router'
+
+
+const routes = [
+    {name: 'Home', component: Home, path: '/'},
+    {name: 'Signup', component: SignUp, path: '/sign-up'}
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
