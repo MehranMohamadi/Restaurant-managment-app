d3434313cd978948e92a1e205054667516eb5334
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-09-11 16:32:43 +0430

    botton styled

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -16,18 +16,28 @@
 </script>
 
 <style scoped>
-.logo{
-    width: 100px;
-}
-   .register input{
-       width: 300px;
-       height: 40px;
-       padding-left: 20px;
-       display: block;
-       margin-bottom: 30px;
-       margin-right: auto;
-       margin-left: auto;
-       border: 1px solid skyblue;
+    .logo {
+        width: 100px;
+    }
 
-   }
+    .register input {
+        width: 300px;
+        height: 40px;
+        padding-left: 20px;
+        display: block;
+        margin-bottom: 30px;
+        margin-right: auto;
+        margin-left: auto;
+        border: 1px solid skyblue;
+
+    }
+
+    .register button {
+        width: 320px;
+        height: 40px;
+        border: 1px solid skyblue;
+        background-color: skyblue;
+        color: #afff;
+        cursor: pointer;
+    }
 </style>
