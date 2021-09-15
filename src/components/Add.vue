773d1fc15de9362f2df3f615b12409d8c1a501af
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-09-15 19:38:03 +0430

    axios in add

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,43 +1,52 @@
 <template>
-    <Header></Header>
-    <h1>Hello ,welcom to add food page</h1>
-    <form class="add">
-        <input type="text" name="name" placeholder="enter name" v-model="food.name"/>
-        <input type="text" name="rate" placeholder="enter rate" v-model="food.rate"/>
-        <input type="text" name="price" placeholder="enter price" v-model="food.price"/>
-        <button type="button" v-on:click="addfood">Add new Food</button>
-    </form>
+  <Header></Header>
+  <h1>Hello ,welcom to add food page</h1>
+  <form class="add">
+    <input type="text" name="name" placeholder="enter name" v-model="food.name"/>
+    <input type="text" name="rate" placeholder="enter rate" v-model="food.rate"/>
+    <input type="text" name="price" placeholder="enter price" v-model="food.price"/>
+    <button type="button" v-on:click="addfood">Add new Food</button>
+  </form>
 </template>
 
 <script>
-    import Header from "./Header";
+import Header from "./Header";
+import axios from "axios";
 
-    export default {
-        name: "Add",
-        components: {
-            Header
-        },
-        data() {
-            return {
-                food: {
-                    name: '',
-                    rate: '',
-                    price: ''
-                }
-            }
-        },
-        methods: {
-            addfood(){
-                console.warn(this.food)
-            }
-        },
-        mounted() {
-            let user = localStorage.getItem('user-info');
-            if (!user) {
-                this.$router.push({name: 'Signup'})
-            }
-        }
+export default {
+  name: "Add",
+  components: {
+    Header
+  },
+  data() {
+    return {
+      food: {
+        name: '',
+        rate: '',
+        price: ''
+      }
     }
+  },
+  methods: {
+    addfood() {
+      console.warn(this.food);
+      const result = axios.post("http://localhost:3000/food", {
+        name: this.food.name,
+        price: this.food.price,
+        rate: this.food.rate
+      });
+      console.warn('result', result)
+
+
+    }
+  },
+  mounted() {
+    let user = localStorage.getItem('user-info');
+    if (!user) {
+      this.$router.push({name: 'Signup'})
+    }
+  }
+}
 </script>
 
 <style scoped>
